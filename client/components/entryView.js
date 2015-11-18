6065216929b14d06631b8916c246e6ed09ef6ad3
Sarah Yu <sarah.m.yu88@gmail.com>
2015-11-17 16:20:23 -0800

    Modify entry view

--- client/components/entryView.js
+++ client/components/entryView.js
@@ -20,35 +20,14 @@ const TVEntry = React.createClass({
     var iframe = this.parseHtml(this.props.data.partner_player); 
     $(id).append(iframe);
   },
-  selectImage: function(imageArray) {
-    for(var i = 0; i < imageArray.length; i++) {
-      if(imageArray[i].type.usage_type==="iPhone-Medium") {
-        return imageArray[i].url;
-      } else if(imageArray[i].type.usage_type==="iPad-Small") {
-        return imageArray[i].url;
-      } else if (imageArray[i].type.usage_type==="program-mezzanine-16x9") {
-        return imageArray[i].url;
-      }
-    }
-  },
   render: function() {
-    if(this.props.data.cove_id) {
-      return (
-        <div className="ui segment">
-          <img src={this.props.data.associated_images.length > 0 ? this.selectImage(this.props.data.associated_images) : ""} />
-          <a href={'http://159.203.243.222/tv/programs-id/?program_id=' + this.props.data.cove_id}><h3 className="ui header react-link">{this.props.data.title}</h3></a>
-        </div>
-        );
-    } else {
-      return (
-        <div className="ui segment">
-          <div id={this.props.data.id}></div>
-          <h3>{this.props.data.title}</h3>
-          <div>{this.props.data.short_description}</div>
-        </div>
+    return (
+      <div className="ui segment">
+        <img src={this.props.data.associated_images.length > 0 ? this.selectImage(this.props.data.associated_images) : ""} />
+        <a href={'http://159.203.243.222/tv/programs-id/?program_id=' + this.props.data.cove_id}><h3 className="ui header react-link">{this.props.data.title}</h3></a>
+      </div>
       );
     }
-  }
 });
 
 module.exports = TVEntry;
